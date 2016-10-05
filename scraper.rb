4d0114f60b676039425c398dce7fd5d5a8e0f665
Tony Bowden <tony@tmtm.com>
2016-10-05 03:37:33 +0100

    Add AllMembersPage class
    Add a class to represent the "Abgeordnete zum Nationalrat seit 1920"
    page, listing (currently) 1778 members since 1920.

--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,75 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
+require 'field_serializer'
 require 'nokogiri'
 require 'pry'
 require 'scraperwiki'
 
+# require 'open-uri/cached'
 require 'scraped_page_archive/open-uri'
+
+class String
+  def tidy
+    gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+class Page
+  include FieldSerializer
+
+  def initialize(url)
+    @url = url
+  end
+
+  def noko
+    @noko ||= Nokogiri::HTML(open(url).read)
+  end
+
+  private
+
+  attr_reader :url
+
+  def absolute_url(rel)
+    return if rel.to_s.empty?
+    URI.join(url, URI.encode(URI.decode(rel)))
+  end
+end
+
+class AllMembersPage < Page
+  field :members do
+    member_table.css('tr').drop(1).map do |tr|
+      td = tr.css('td')
+      {
+        name:    td[0].text.tidy,
+        faction: td[1].text.split(/,\s+/).map(&:tidy),
+        terms:   td[2].text.split(/,\s+/).map(&:tidy),
+        state:   td[3].text.split(/,\s+/).map(&:tidy),
+        url:     absolute_url(td[0].css('a/@href').text),
+      }
+    end
+  end
+
+  private
+
+  def member_table
+    mt = noko.css('.tabelle') or raise 'No member table'
+    raise 'Too many member tables' if mt.count > 1
+    mt
+  end
+end
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
 LIST_PAGE = 'https://www.parlament.gv.at/WWER/NR/ABG/index.shtml?xdocumentUri=%2FWWER%2FNR%2FABG%2Findex.shtml&R_BW=BL&GP=ALLE&BL=ALLE&STEP=&FR=ALLE&M=M&NRBR=NR&FBEZ=FW_004&WK=ALLE&requestId=22CCE571C5&jsMode=&LISTE=&W=W&letter=&WP=ALLE&listeId=4&R_WF=FR'
-LINK_SELECTOR = '.tabelle tr a[href*="/WWER/"]/@href'
 
-# For now, only archive the pages. Parsing them can come later.
-noko = noko_for(LIST_PAGE)
-noko.css(LINK_SELECTOR).each do |href|
-  _ = noko_for(URI.join(LIST_PAGE, href.text))
+data = AllMembersPage.new(LIST_PAGE).to_h
+warn "Found #{data[:members].count} members"
+
+# For now, only archive the member pages. Parsing them can come later.
+data[:members].each do |mem|
+  _ = noko_for(mem[:url])
 end
-
